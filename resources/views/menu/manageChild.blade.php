b35e654c8774de49ca161fef1b633b6ea4bb605b
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2020-10-27 14:45:49 +0500

    initial commit

---
+++ resources/views/menu/manageChild.blade.php
@@ -0,0 +1,9 @@
+@foreach($childs as $child)
+   <li>
+       {{ $child->title }}
+   @if(count($child->childs))
+            @include('manageChild',['childs' => $child->childs])
+        @endif
+   </li>
+@endforeach
+</ul>
